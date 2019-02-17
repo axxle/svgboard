91d3064472cd4b9d8495f2520f19a68486f8bf52
Vadik <kvv0911@gmail.com>
2019-02-17 10:57:27 +0300

    dirty commit4

--- src/main/webapp/svgboard.js
+++ src/main/webapp/svgboard.js
@@ -157,3 +157,29 @@ function getScaleValues(wheelDelta, mouseX, mouseY) {
 	currentScaleIndex = nextScaleIndex;
 	return shiftedArr;
 }
+
+/*Получение сохраненных настроек доски*/
+function getBoardSettingsFromServer() {
+    //хочу получать настройки с сервера при обновлении страницы, или по требованию
+    var width = "1920";
+    var height = "908";
+    var viewbox = "0 0 1920 908";
+    //засетить в нужные аттрибуты
+    //var svg = document.getElementById(id);
+    //svg.setAttribute("viewBox", viewbox);
+    console.log("Получение настроек доски с сервера: "
+        , "width=", width, " height=", height, " viewbox=", viewbox);
+}
+
+/*Сохранение настроек доски на сервере*/
+function saveBoardSetting(settings) {
+    //хочу отправлять на сервер настройки, с задержкой 1 сек,
+    // со сбросом таймера при последующих изменениях:
+    //	1. При перетаскивании, один раз после отпускания кнопки
+    //	2. При масштабировании
+    var width = document.getElementById(id).getAttribute("width");
+    var height = document.getElementById(id).getAttribute("height");
+    var viewbox = document.getElementById(id).getAttribute("viewBox");
+    console.log("Отправка настроек доски на сервер: "
+        , "width=", width, " height=", height, " viewbox=", viewbox);
+}
