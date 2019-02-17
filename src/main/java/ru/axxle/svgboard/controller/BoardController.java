353f715a67bd9e689900d69bb8ab626db21d6c78
Vadik <kvv0911@gmail.com>
2019-02-17 15:39:26 +0300

    dirty15

--- src/main/java/ru/axxle/svgboard/controller/BoardController.java
+++ src/main/java/ru/axxle/svgboard/controller/BoardController.java
@@ -2,6 +2,7 @@ package ru.axxle.svgboard.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,7 +31,7 @@ public class BoardController {
 
     @RequestMapping(
             value = BOARD_URL,
-            method = RequestMethod.POST)
+            method = RequestMethod.POST, produces = { MediaType.APPLICATION_JSON_VALUE})
     ResponseEntity<Board> saveBoardSettings(@RequestBody Board board) {
         Board savedBoard = boardService.saveBoard(board);
         return new ResponseEntity<Board>(savedBoard, HttpStatus.OK);
